3c153d3b01323835f135710d857c96237519b5b5
Denis Fernando <denisferoli@gmail.com>
2023-06-14 00:32:06 -0300

    Update acessar.php

--- acessar.php
+++ acessar.php
@@ -1,7 +1,15 @@
 <?php
 
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
 
+        $password = $_POST["password"];
+        $user = $_POST["login"];
 
-
+        if ($password == 123) {
+            header("Location: menu.html");
+        } else {
+            echo "Senha incorreta.";
+        };
+    };
 
 ?>
